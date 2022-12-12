9362a798bdd906df66ba98c51c08747070f78d2d
olegsvs <oleg.texet@gmail.com>
2022-12-12 04:25:17 +0300

    STT: switch to yandex speechkit

--- stt.py
+++ stt.py
@@ -1,50 +1,67 @@
-import json
+from speechkit import Session, RecognitionLongAudio
+from speechkit.auth import generate_jwt
+import time
 import os
+import json
 import subprocess
 from datetime import datetime
 
-from vosk import KaldiRecognizer, Model
-
+bucket_name = "voice-bucket"
+service_account_id = ""
+key_id = ""
+private_key = ""
+jwt = generate_jwt(service_account_id, key_id, private_key)
+session = Session.from_jwt(jwt)
+recognize_long_audio = RecognitionLongAudio(session, service_account_id, bucket_name)
 
 class STT:
     default_init = {
-        "model_path": "models/vosk",
         "sample_rate": 16000,
     }
 
     def __init__(self,
-                 model_path=None,
                  sample_rate=None,
                  ffmpeg_path=None
                  ) -> None:
-        self.model_path = model_path if model_path else STT.default_init["model_path"]
         self.sample_rate = sample_rate if sample_rate else STT.default_init["sample_rate"]
 
-        model = Model(self.model_path)
-        self.recognizer = KaldiRecognizer(model, self.sample_rate)
-        self.recognizer.SetWords(True)
 
-
-    def audio_to_text(self, audio_file_name=None) -> str:
+    async def audio_to_text(self, audio_file_name=None) -> str:
+        f = open("voice_cache/" + os.path.basename(audio_file_name) + "processing.wav", "w")
         process = subprocess.Popen(
             ["ffmpeg",
              "-loglevel", "quiet",
              "-i", audio_file_name,
              "-ar", str(self.sample_rate),
              "-ac", "1",
-             "-f", "s16le",
+             "-acodec", "pcm_s16le",
+             "-f", "wav",
              "-"
              ],
-            stdout=subprocess.PIPE
-                                   )
+            stdout=f
+        )
 
+        f.close()
+        process.wait()
+        recognize_long_audio.send_for_recognition(
+            "voice_cache/" + os.path.basename(audio_file_name) + "processing.wav", audioEncoding='LINEAR16_PCM', sampleRateHertz='16000',
+            audioChannelCount=1, rawResults=False, profanityFilter='false', literatureText='true',
+        )
+        try:
+            os.remove("voice_cache/" + os.path.basename(audio_file_name) + "processing.wav")
+        except:
+            pass
         while True:
-            data = process.stdout.read(4000)
-            if len(data) == 0:
+            time.sleep(2)
+            if recognize_long_audio.get_recognition_results():
                 break
-            if self.recognizer.AcceptWaveform(data):
-                pass
 
-        result_json = self.recognizer.FinalResult()
-        result_dict = json.loads(result_json)
-        return result_dict["text"]
+        data = recognize_long_audio.get_data()
+        text = recognize_long_audio.get_raw_text()
+        result = text[0]
+        for letter in text[1:]:
+            if letter.isupper():
+                result += f' {letter}'
+            else:
+                result += letter
+        return result
